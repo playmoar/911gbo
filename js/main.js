36b45a4050e13897b8c2f594b6e18094d493fdae
playmoar <playmoar@icloud.com>
2020-03-29 18:01:01 +0300

    tabs ready

--- js/main.js
+++ js/main.js
@@ -32,3 +32,38 @@ petrolConsumption.oninput = function () {
 }
 
 calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
+
+const tab1line = document.getElementById("tab1line");
+const tab1text = document.getElementById("tab1text");
+const tab1content = document.getElementById("tab1content");
+const tab2line = document.getElementById("tab2line");
+const tab2text = document.getElementById("tab2text");
+const tab2content = document.getElementById("tab2content");
+
+
+tab2text.addEventListener("click", e => {
+  e.preventDefault();
+
+  document.getElementById("tab1line").classList.remove("active-line");
+  document.getElementById("tab1text").classList.remove("active-gen");
+  document.getElementById("tab1content").classList.remove("active");
+  document.getElementById("tab2line").classList.add("active-line");
+  document.getElementById("tab2text").classList.add("active-gen");
+  document.getElementById("tab2content").classList.add("active");
+
+
+});
+
+tab1text.addEventListener("click", e => {
+  e.preventDefault();
+
+  document.getElementById("tab2line").classList.remove("active-line");
+  document.getElementById("tab2text").classList.remove("active-gen");
+  document.getElementById("tab2content").classList.remove("active");
+  document.getElementById("tab1line").classList.add("active-line");
+  document.getElementById("tab1text").classList.add("active-gen");
+  document.getElementById("tab1content").classList.add("active");
+
+
+});
+
