72cb758dd144c075bed8582b8ee26b044dfce354
playmoar <playmoar@icloud.com>
2020-03-29 13:55:19 +0300

    calculator fix

--- js/main.js
+++ js/main.js
@@ -13,22 +13,22 @@ var calculationResultValue = document.getElementById("calculation-result-value")
 
 gasCost.oninput = function () {
   gasCostOut.innerHTML = this.value;
-  calculationResultValue.innerHTML = (petrolConsumption.value / 100 * dailyMileage.value * 1000 * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * 1000 * gasCost.value).toFixed(0);
+  calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
 }
 
 petrolCost.oninput = function () {
   petrolCostOut.innerHTML = this.value;
-  calculationResultValue.innerHTML = (petrolConsumption.value / 100 * dailyMileage.value * 1000 * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * 1000 * gasCost.value).toFixed(0); calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
+  calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
 }
 
 dailyMileage.oninput = function () {
   dailyMileageOut.innerHTML = this.value;
-  calculationResultValue.innerHTML = (petrolConsumption.value / 100 * dailyMileage.value * 1000 * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * 1000 * gasCost.value).toFixed(0); calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
+  calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
 }
 
 petrolConsumption.oninput = function () {
   petrolConsumptionOut.innerHTML = this.value;
-  calculationResultValue.innerHTML = (petrolConsumption.value / 100 * dailyMileage.value * 1000 * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * 1000 * gasCost.value).toFixed(0); calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
+  calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
 }
 
 calculationResultValue.innerHTML = ((petrolConsumption.value / 100 * dailyMileage.value * petrolCost.value - petrolConsumption.value / 100 * dailyMileage.value * gasCost.value) * 365).toFixed(0);
